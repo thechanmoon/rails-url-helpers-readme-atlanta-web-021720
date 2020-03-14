4ebe747e3858c81b0af4b727ffd9d1699c5abb02
tiburon <thechanmoon@gmail.com>
2020-03-14 18:24:53 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# t.string   "title"
+# t.text     "description"
+# t.datetime "created_at",  null: false
+# t.datetime "updated_at",  null: false
+Student.create([{ first_name: "Rick", last_name: "Sanchez" }, { first_name: "Morty", last_name: "Smith" }])
+Post.create(title: 'TEST', description: 'DESCRIPTION', created_at: '03142020', updated_at: '03142020')
